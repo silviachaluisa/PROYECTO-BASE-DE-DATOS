d94ef837bd032a54e341a1666efe9d06861a77d7
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-13 13:34:45 -0500

    modificacion del script

--- script.sql
+++ script.sql
@@ -1,2 +1,11 @@
 Use sql5681662;
 -- crear loas tablas
+create table Usuarios(
+    id int primary key AUTO_INCREMENT,
+    usuario VARCHAR(30) not null, 
+    contrasena VARCHAR(15) not NULL,
+    cedula BIGINT not null UNIQUE,
+    correo text not null UNIQUE,
+    contacto BIGINT not null,
+    fecha_nacimiento date not null,
+);
