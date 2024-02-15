0a458ec2d94b059be21fba7605dd7f11ae494364
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-15 15:07:35 -0500

    Cambio Silvia

--- script.sql
+++ script.sql
@@ -1,12 +1,12 @@
 Use sql5681662;
--- crear loas tablas
+-- crear las tablas
 create table TipoUsuario{
     Id_Usuario int PRIMARY KEY not null,
-    tipo_usuario text not null check(tipo_usuario in ("Administrador", "Ascesor", "Empleado", "Usuario"))
+    Tipo_Usuario text not null check(tipo_usuario in ("Administrador", "Ascesor", "Empleado", "Usuario"))
 
 }
-create table Usuarios(
-    Id_Usuario int primary key AUTO_INCREMENT,
+create table Clientes(
+    Id_Cliente int primary key AUTO_INCREMENT,
     Nombre VARCHAR(30) not null, 
     Cédula BIGINT not null UNIQUE,
     Contacto BIGINT not null,
@@ -14,35 +14,30 @@ create table Usuarios(
     Fecha_Nacimiento date not null,
     Contraseña VARCHAR(50) not NULL,
     Dirección TEXT not null
-    tipo_usuario int,
-    Foreign Key (tipo_usuario) REFERENCES TipoUsuario(Id_Usuario)
+    Tipo_Usuario int,
+    Foreign Key (Tipo_Usuario) REFERENCES TipoUsuario(Id_Usuario)
 );
 
 
 
 
 create table Vehiculos(
-    id_vehiculo int PRIMARY KEY not null,
-    marca text not null,
-    modelo text not null,
-    añio int not null,
-    tipo varchar(50) not null,
-    color varchar(30) not null,
-    placa text not null UNIQUE,
-    id_propietario int,
-    id_conductor int
-    Foreign Key (id_propietario) REFERENCES Propietarios(id_propietario),
-    Foreign Key (id_conductor) REFERENCES Conductores(id_conductor)
+    Placa varchar(10) PRIMARY KEY not null,
+    Marca text not null,
+    Modelo text not null,
+    Año int not null,
+    Color varchar(30) not null,
+    Id_Cliente int,
+    Foreign Key (Id_Cliente) REFERENCES Clientes(Id_Cliente),
+    
 );
 
 create table Mantenimiento(
-    id int PRIMARY KEY not null,
-    id_vehiculo int,
-    fecha date not null,
-    descripcion text not null,
-    costo decimal(5, 2) not null,
-    piezas_reemplazadas text not null,
-    proveedor text not null,
-    garantia VARCHAR(5) not null check(garantia in ("si","no")),
-    fecha_siguiente_mantenimiento date not null
+    Id_Mantenimiento int PRIMARY KEY not null,
+    Placa varchar(10),
+    Fecha_Mantenimiento date not null,
+    Descripcion text not null,
+    Costo decimal(5, 2) not null,
+    Garantia VARCHAR(5) not null check(garantia in ("si","no")),
+    Fecha_Siguiente_Mantenimiento date not null
 )
